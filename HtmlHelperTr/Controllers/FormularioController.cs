e216b5571b57ccf20e0a8e4a687ad05eeea32fdf
Srestituyo <Srestituyo@users.noreply.github.com>
2019-03-30 13:31:59 -0400

    HtmlHelper
    Tarea sobre HtmlHelper

---
+++ HtmlHelperTr/Controllers/FormularioController.cs
@@ -0,0 +1,37 @@
+﻿using HtmlHelperTr.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HtmlHelperTr.Controllers
+{
+    public class FormularioController : Controller
+    {
+        // GET: Formulario
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index( Formulario formulario)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                return View("Detalle", formulario);
+            }
+            else {
+                return View(formulario);
+            }
+
+            
+        }
+
+        public ActionResult Detalle() {
+            return View();
+        }
+    }
+}
---
+++ HtmlHelperTr/Models/Formulario.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.ModelBinding;
+
+namespace HtmlHelperTr.Models
+{
+    public class Formulario
+    {
+        [Required]
+        public String Cedula { get; set; }
+        [Required]
+        public String Nombre { get; set; }
+        [Required]
+        public String Apellido { get; set; }
+        [Required,Range(15,99)]
+        public int Edad { get; set; }
+        public String Telefono { get; set; }
+        [Required,DataType(DataType.EmailAddress),EmailAddress]
+        public String Correo { get; set; }
+        public String Genero { get; set; }
+        public String Estado { get; set; }
+        public bool Hobbys { get; set; }
+
+    }
+
+
+
+    public enum Gender {
+        Masculino,
+        Femenino
+    }
+}
